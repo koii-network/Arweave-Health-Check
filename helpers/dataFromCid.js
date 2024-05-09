d577d8d1eae57ee941ada140e133262af561b302
yiqun Liang <herman@koii.network>
2024-05-09 18:48:14 +0100

    fix ipfs

--- helpers/dataFromCid.js
+++ helpers/dataFromCid.js
@@ -1,36 +1,31 @@
 const axios = require('axios');
 
-module.exports = async (cid, fileName) => {
-  console.log('CID', cid);
-  let headers = {
-    headers: {
-      'User-Agent':
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 ',
-    },
-  };
-  const retryDelay = 5000;
-  const url = `https://${cid}.ipfs.sphn.link/${fileName}`;
-  let maxRetries = 3;
-  for (let attempt = 1; attempt <= 3; attempt++) {
-    try {
-      const response = await axios.get(url);
-      if (response.status === 200) {
-        return response;
-      } else {
-        // console.log(`Attempt loading IPFS ${attempt}: Received status ${response.status}`);
-      }
-    } catch (error) {
-      console.log(`Attempt loading IPFS ${attempt} failed: ${error.message}`);
-      if (attempt < maxRetries) {
-        // console.log(`Waiting for ${retryDelay / 1000} seconds before retrying...`);
-        await sleep(retryDelay);
-      } else {
-        return false; // Rethrow the last error
+module.exports = async (cid, fileName, maxRetries = 4, retryDelay = 3000) => {
+  const urllist = [
+    `https://${cid}.ipfs.sphn.link/${fileName}`,
+    `https://${cid}.ipfs.4everland.io/${fileName}`,
+    `https://cloudflare-ipfs.com/ipfs/${cid}/${fileName}`,
+    `https://${cid}.ipfs.dweb.link/${fileName}`,
+  ];
+  console.log(urllist);
+  for (const url of urllist) {
+    console.log(`Trying URL: ${url}`);
+    for (let attempt = 1; attempt <= maxRetries; attempt++) {
+      try {
+        const response = await axios.get(url);
+        if (response.status === 200) {
+          return response;
+        } else {
+          // console.log(`Attempt ${attempt} at IPFS ${url}: status ${response.status}`);
+        }
+      } catch (error) {
+        // console.log(`Attempt ${attempt} at IPFS ${url} failed: ${error.message}`);
+        if (attempt < maxRetries) {
+          await sleep(retryDelay);
+        }
       }
     }
   }
-  
-  async function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
+  console.log("Attempted all IPFS sites failed");
+  return false; 
 };

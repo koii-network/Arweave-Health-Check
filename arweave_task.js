a70c8918f1a84628c70e05fef77e90cb60d126df
yiqun Liang <herman@koii.network>
2024-05-30 14:56:54 +0100

    Reduce fetches clear unused function

--- arweave_task.js
+++ arweave_task.js
@@ -51,64 +51,64 @@ const run = async round => {
   return result;
 };
 
-uploadIPFS = async function (data, round) {
-  let proofPath = `proofs.json`;
-  let basePath = '';
-  try {
-    basePath = await namespaceWrapper.getBasePath();
-    fs.writeFileSync(`${basePath}/${proofPath}`, JSON.stringify(data));
-  } catch (err) {
-    console.log(err);
-  }
-  let attempts = 0;
-  let maxRetries = 3;
-  if (storageClient) {
-    while (attempts < maxRetries) {
-      let proof_cid;
-      try {
-        // const basePath = await namespaceWrapper.getBasePath();
-        // let file = await getFilesFromPath(`${basePath}/${path}`);
-        // console.log(`${basePath}/${proofPath}`);
-        const userStaking = await namespaceWrapper.getSubmitterAccount();
-        console.log(`Uploading ${basePath}/${proofPath}`);
-        const fileUploadResponse = await client.uploadFile(`${basePath}/${proofPath}`,userStaking);
-        console.log(`Uploaded ${basePath}/${proofPath}`);
-        try{
-          proof_cid = fileUploadResponse.cid;
-        }catch(err){
-          proof_cid = null;
-          console.log('error getting CID', err);
-        }
+// uploadIPFS = async function (data, round) {
+//   let proofPath = `proofs.json`;
+//   let basePath = '';
+//   try {
+//     basePath = await namespaceWrapper.getBasePath();
+//     fs.writeFileSync(`${basePath}/${proofPath}`, JSON.stringify(data));
+//   } catch (err) {
+//     console.log(err);
+//   }
+//   let attempts = 0;
+//   let maxRetries = 3;
+//   if (storageClient) {
+//     while (attempts < maxRetries) {
+//       let proof_cid;
+//       try {
+//         // const basePath = await namespaceWrapper.getBasePath();
+//         // let file = await getFilesFromPath(`${basePath}/${path}`);
+//         // console.log(`${basePath}/${proofPath}`);
+//         const userStaking = await namespaceWrapper.getSubmitterAccount();
+//         console.log(`Uploading ${basePath}/${proofPath}`);
+//         const fileUploadResponse = await client.uploadFile(`${basePath}/${proofPath}`,userStaking);
+//         console.log(`Uploaded ${basePath}/${proofPath}`);
+//         try{
+//           proof_cid = fileUploadResponse.cid;
+//         }catch(err){
+//           proof_cid = null;
+//           console.log('error getting CID', err);
+//         }
         
 
-        // console.log(`CID: ${proof_cid}`);
-        console.log('Arweave healthy list to IPFS: ', proof_cid);
+//         // console.log(`CID: ${proof_cid}`);
+//         console.log('Arweave healthy list to IPFS: ', proof_cid);
 
-        try {
-          fs.unlinkSync(`${basePath}/${proofPath}`);
-        } catch (err) {
-          console.error(err);
-        }
-        return proof_cid;
-      } catch (err) {
-        console.log('error uploading to IPFS, trying again', err);
-        attempts++;
-        if (attempts < maxRetries) {
-          console.log(
-            `Waiting for 10 seconds before retrying... Attempt ${
-              attempts + 1
-            }/${maxRetries}`,
-          );
-          await new Promise(resolve => setTimeout(resolve, 10000)); // 10s delay
-        } else {
-          console.log('Max retries reached, exiting...');
-        }
-      }
-      break;
-    }
-  } else {
-    console.log('NODE DO NOT HAVE ACCESS TO KOII STORAGE');
-  }
-};
+//         try {
+//           fs.unlinkSync(`${basePath}/${proofPath}`);
+//         } catch (err) {
+//           console.error(err);
+//         }
+//         return proof_cid;
+//       } catch (err) {
+//         console.log('error uploading to IPFS, trying again', err);
+//         attempts++;
+//         if (attempts < maxRetries) {
+//           console.log(
+//             `Waiting for 10 seconds before retrying... Attempt ${
+//               attempts + 1
+//             }/${maxRetries}`,
+//           );
+//           await new Promise(resolve => setTimeout(resolve, 10000)); // 10s delay
+//         } else {
+//           console.log('Max retries reached, exiting...');
+//         }
+//       }
+//       break;
+//     }
+//   } else {
+//     console.log('NODE DO NOT HAVE ACCESS TO KOII STORAGE');
+//   }
+// };
 
 module.exports = run;
--- helpers/dataFromCid.js
+++ helpers/dataFromCid.js
@@ -1,40 +1,28 @@
 const axios = require('axios');
 
-module.exports = async (cid, fileName, maxRetries = 4, retryDelay = 3000) => {
-  const urllist = [
-    `https://${cid}.ipfs.4everland.io/${fileName}`,
-    `https://cloudflare-ipfs.com/ipfs/${cid}/${fileName}`,
-    `https://${cid}.ipfs.dweb.link/${fileName}`,
-  ];
-  const {KoiiStorageClient} = require('@_koii/storage-task-sdk');
-  const client = new KoiiStorageClient(undefined, undefined, true);
-  try {
-    const blob = await client.getFile(cid, fileName);
-    const text = await blob.text(); // Convert Blob to text
-    const data = JSON.parse(text); // Parse text to JSON
-    return data;
-  }  catch (error) {
-    console.log(`Error fetching file from Koii IPFS: ${error.message}`);
-  }
-  console.log(urllist);
-  for (const url of urllist) {
-    console.log(`Trying URL: ${url}`);
-    for (let attempt = 1; attempt <= maxRetries; attempt++) {
+module.exports = async (cid, fileName) => {
+    const urllist = [
+      `https://${cid}.ipfs.w3s.link/${fileName}`
+    ];
+    try {
+      const client = new KoiiStorageClient(undefined, undefined, true);
+      const blob = await client.getFile(cid, fileName);
+      const text = await blob.text(); // Convert Blob to text
+      const data = JSON.parse(text); // Parse text to JSON
+      return data;
+    }  catch (error) {
+      console.log(`Error fetching file from Koii IPFS: ${error.message}`);
+    }
+    for (const url of urllist) {
+      console.log(`Trying URL: ${url}`);
       try {
         const response = await axios.get(url);
         if (response.status === 200) {
-          return response;
-        } else {
-          // console.log(`Attempt ${attempt} at IPFS ${url}: status ${response.status}`);
+          return response.data;
         }
       } catch (error) {
-        // console.log(`Attempt ${attempt} at IPFS ${url} failed: ${error.message}`);
-        if (attempt < maxRetries) {
-          await sleep(retryDelay);
-        }
       }
     }
-  }
-  console.log("Attempted all IPFS sites failed");
-  return false; 
+    console.log("Attempted all IPFS sites failed");
+    return null; 
 };
